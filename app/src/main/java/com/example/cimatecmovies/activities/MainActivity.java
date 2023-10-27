b08edbe961e254add162ce3d36e92ac4eadb8cc7
João Pedro de Lima <48960188+jotalima72@users.noreply.github.com>
2023-10-27 17:00:05 -0300

    problema pra vericar o RA

--- app/src/main/java/com/example/cimatecmovies/activities/MainActivity.java
+++ app/src/main/java/com/example/cimatecmovies/activities/MainActivity.java
@@ -3,59 +3,83 @@ package com.example.cimatecmovies.activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.cimatecmovies.DAO.PlaylistDAO;
 import com.example.cimatecmovies.R;
 import com.example.cimatecmovies.model.Movie;
 import com.example.cimatecmovies.model.Playlist;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
-
-    Playlist playlist = new Playlist("antonho");
-    Movie movie = new Movie("O voo do passaro que nao sabia voar", 1925);
-
-
+    private EditText raText;
+    private Button raButton;
+    DatabaseReference data = FirebaseDatabase.getInstance().getReference().child("users");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        playlist.setMovies(movie);
-        ref.child("users").child("018664138").setValue(playlist);
-        getData();
-    }
-    public void getData(){
-        DatabaseReference data = FirebaseDatabase.getInstance().getReference().child("users");
-        data.addValueEventListener(new ValueEventListener() {
+        raText = findViewById(R.id.raText);
+        raButton = findViewById(R.id.raButton);
+
+        raButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                ArrayList<Playlist> p = new ArrayList<>();
-                for (DataSnapshot sp: snapshot.getChildren()) {
-                Playlist temp = new Playlist();
-                    for(DataSnapshot snapMovie : sp.child("movies").getChildren()){
-                        temp.setMovies(snapMovie.getValue(Movie.class));
+            public void onClick(View v) {
+                String ra = raText.getText().toString();
+                PlaylistDAO dbPlaylists = new PlaylistDAO();
+                OnCompleteListener<DataSnapshot> listener = new OnCompleteListener<DataSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+                        String user = "";
+                        if (task.isSuccessful()){
+                            DataSnapshot dataRes = task.getResult();
+                            for (DataSnapshot sp : dataRes.getChildren()) {
+                                String result = sp.getKey();
+                                System.out.println(result);
+                                if(result.equals(ra)){
+                                    user = result;
+                                    break;
+                                }
+                            }
+                            if(user.isEmpty()){
+                                System.out.println("chamar tela de nome");
+                            }
+                            else {
+                                System.out.println("chamar lista de playlists");
+                            }
+                        }
+
+
                     }
-                    temp.setCreatorsName(sp.child("creatorsName").getValue(String.class));
-                    temp.setLikes(sp.child("likes").getValue(int.class));
-                    p.add(temp);
-                }
-                p.forEach(pl -> System.out.println(pl.toString()));
+                };
+                data.get().addOnCompleteListener(listener);
 
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
+//                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//                startActivity(intent);
             }
         });
+//        playlist.setMovies(movie);
+//        ref.child("users").child("018664138").setValue(playlist);
+//        getData();
     }
 
-
 }
+
+
+
---
+++ app/src/main/java/com/example/cimatecmovies/activities/Playlist_lists.java
@@ -0,0 +1,16 @@
+package com.example.cimatecmovies.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cimatecmovies.R;
+
+public class Playlist_lists extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playlist_lists);
+    }
+}
